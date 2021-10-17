d4805039892f37df8765c96cf0ff64aaf79a4edf
shearpaladin <sandy6@ualberta.ca>
2021-10-16 19:46:49 -0600

    fixing merge conflicts #2

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,13 +1,6 @@
-from django.urls import path 
-from frontend.views import Index 
+from django.urls import path
+from frontend.views import Index
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
-<<<<<<< HEAD
-<<<<<<< HEAD
-    path('feed', Index.as_view(), name='feed'),
-=======
->>>>>>> 03b29879a368a6a8752038b7ccbe3af6930724d3
-=======
->>>>>>> 03b29879a368a6a8752038b7ccbe3af6930724d3
 ]
