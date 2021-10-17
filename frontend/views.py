87bae2457d999fc81172df9877ca40916fa84c67
shearpaladin <sandy6@ualberta.ca>
2021-10-16 19:59:22 -0600

    fixed frontend/views.py

--- frontend/views.py
+++ frontend/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 
+
 class Index(View):
-<<<<<<< HEAD
-<<<<<<< HEAD
-    def get(self, request, *args, **kwargs):
-=======
     def get(self, request):
->>>>>>> 03b29879a368a6a8752038b7ccbe3af6930724d3
-=======
-    def get(self, request):
->>>>>>> 03b29879a368a6a8752038b7ccbe3af6930724d3
         return render(request, 'frontend/index.html')
-    
+
