dfbe594436a5838e36067b1abb0d760675c5e43d
Mehrshad Sahebsara <msahebsara@live.ca>
2021-10-18 20:51:58 -0600

    Add basic pagination for posts

--- backend/api.py
+++ backend/api.py
@@ -1,14 +1,15 @@
 from backend.models import Post 
-from rest_framework import viewsets, permissions 
+from rest_framework import viewsets, permissions, pagination
 from .serializers import PostSerializer 
 
-# Viewset for Post
 class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [
         permissions.IsAuthenticated
     ]
 
     serializer_class = PostSerializer
+
+    pagination_class = pagination.PageNumberPagination
 
     def get_queryset(self):
         return self.request.user.posts.all()
