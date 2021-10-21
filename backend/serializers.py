84a421b066f3069940c97c2333a318f1ddbe2348
amahesh <amahesh@ntwist.com>
2021-10-21 15:50:03 -0600

    read_only_fields added

--- backend/serializers.py
+++ backend/serializers.py
@@ -12,4 +12,5 @@ class PostSerializer(serializers.ModelSerializer):
 class CommentSerializer(serializers.ModelSerializer): 
     class Meta: 
         model = Comment
-        fields = ['uuid', 'commentType', 'author', 'post', 'comment', 'contentType', 'published']
+        fields = '__all__'
+        read_only_fields = ('uuid', 'published')
