cf7c041d08a39691a794ef9b0c8fcb80dbd37228
flyrobot27 <stevenheung.ca@gmail.com>
2021-10-27 20:24:35 -0600

    author api fixed. POST now updates profiles and removed PUT method

--- author/api.py
+++ author/api.py
@@ -1,27 +1,33 @@
+from rest_framework.generics import get_object_or_404
 from author.models import Author
 from .serializer import AuthorSerializer 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets, status
+from django.http import HttpRequest
 from rest_framework.response import Response
 # from rest_framework.decorators import action
 from django.core.paginator import Paginator
+from django.db.models import F
 from knox.auth import TokenAuthentication
-
+from urllib.parse import urlparse
 
 # Viewset for Author
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.exclude(user__isnull=True)
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    #authentication_classes = (TokenAuthentication,)
+    #permission_classes = (IsAuthenticated,)
 
     serializer_class = AuthorSerializer
 
     # GET all authors
-    def list(self, request, *args, **kwargs):
+    def list(self, request: HttpRequest):
         try:
             page = request.GET.get('page', 'None')
             size = request.GET.get('size', 'None')
             author_list = self.get_queryset()
+
+            # Swap the id fields to url
+            author_list.update(id=F('url'))
 
             if(page == "None" or size == "None"):
                 serializer = AuthorSerializer(author_list, many=True)
@@ -29,7 +35,7 @@ class AuthorViewSet(viewsets.ModelViewSet):
                 paginator = Paginator(author_list, size)
                 result_page = paginator.get_page(page)
                 serializer = AuthorSerializer(result_page, many=True)
-           
+
             response = {
                 "type": "authors",
                 "items": serializer.data
@@ -43,51 +49,28 @@ class AuthorViewSet(viewsets.ModelViewSet):
 
 
     # GET using author id
-    def retrieve(self, request, *args, **kwargs):
-        try:
-            return super().retrieve(request, *args, **kwargs)
-        except:
-            response = {
-                "message": "Record not found"
-            }
-            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+    def get_author(self, request: HttpRequest, author_id=None):
+        query = self.get_queryset().filter(id=author_id)
+
+        if not query.exists():
+            return Response({"detail": "Author not found or is not local."}, status=status.HTTP_404_NOT_FOUND)
+
+        query.update(id=F('url'))
 
 
-    # POST
-    def create(self, request, *args, **kwargs):
-        try:
-            new_author = Author(
-                type=request.data["type"],
-                id=request.data["id"],
-                user=request.user,
-                host=request.data["host"],
-                displayName=request.data["displayName"],
-                url=request.data["url"],
-                github=request.data["github"],
-                profileImage=request.data["profileImage"]
-            )
-
-            new_author.save()
-
-            return Response(status.HTTP_201_CREATED)
-
-        except Exception as e:
-            response = {
-                "message": "Record not created",
-                "detail": e.args
-            }
-            return Response(response,status.HTTP_400_BAD_REQUEST)
-
-
-    # PUT
-    def update(self, request, pk=None):
+    # POST and update author's profile
+    def update(self, request: HttpRequest, author_id=None):
         try:
             for key in request.data.keys():
-                author = Author.objects.get(id=pk)
+                author = Author.objects.get(id=author_id)
                 if(key=="url"):
+                    if not self.validate_url(request.data[key]):
+                        return Response({"detail": "url format is invalid"}, status=status.HTTP_400_BAD_REQUEST)
                     author.url=request.data[key]
 
                 elif(key=="host"):
+                    if not self.validate_url(request.data[key]):
+                        return Response({"detail": "host format is invalid"}, status=status.HTTP_400_BAD_REQUEST)
                     author.host=request.data[key]
 
                 elif(key=="displayName"):
@@ -111,3 +94,11 @@ class AuthorViewSet(viewsets.ModelViewSet):
                 "detail": "Incorrect data keys"
             }
             return Response(response,status.HTTP_400_BAD_REQUEST)
+
+    # validate if the url is the correct format
+    def validate_url(self, url: str) -> bool:
+        try:
+            result = urlparse(url)
+            return all([result.scheme, result.netloc])
+        except:
+            return False
--- author/urls.py
+++ author/urls.py
@@ -1,8 +1,8 @@
 # from .views import ListAuthors
-from django.urls.conf import path
+from django.urls.conf import path, re_path
 from .api import AuthorViewSet
 
 urlpatterns = [
     path("authors/", AuthorViewSet.as_view({"get": "list"})),
-    path("author/<str:pk>/", AuthorViewSet.as_view({"get": "retrieve", "post": "create"}))
+    re_path(r'^author/(?P<author_id>\w+)/?$', AuthorViewSet.as_view({"get": "get_author", "post": "update"}))
 ]
