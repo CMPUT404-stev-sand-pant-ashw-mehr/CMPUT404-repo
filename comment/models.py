85cb2def02feca6404cefbf1bdccddf6c17f1a44
flyrobot27 <stevenheung.ca@gmail.com>
2021-10-28 16:58:03 -0600

    Comment Serializer finished

--- comment/models.py
+++ comment/models.py
@@ -9,7 +9,7 @@ class Comment(models.Model):
     # ID of the Comment (UUID)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     
-    commentType = models.CharField(max_length=255, default = "comment")
+    type = models.CharField(max_length=255, default = "comment")
     
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     
