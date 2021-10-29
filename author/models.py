d681c9283950a10d468eda226a749120b6fdd5ed
flyrobot27 <stevenheung.ca@gmail.com>
2021-10-28 19:49:39 -0600

    Merged with Sandy/auth

--- author/models.py
+++ author/models.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 import uuid
 
 class Author(models.Model):
+    type = models.CharField(max_length=255, default="author")
     # ID of the Author
-    id = models.UUIDField(unique=True, editable=False, primary_key=True, default=uuid.uuid4)
+    id = models.CharField(unique=True, primary_key=True, default=uuid.uuid4, max_length=255, null=False, blank=False)
 
     # foreign key to connect with the user auth table in django
     # If this field is null it indicates that the author is not a local user
@@ -26,5 +27,8 @@ class Author(models.Model):
     # Image from a public domain
     profileImage = models.CharField(max_length=255, blank=True)
 
+
+
+    # using this to get full path for author
     def get_full_path(self):
-        return str(self.host) + f"/author/{self.id}"
+        return self.host + f"api/author/{self.id}/"
--- author/serializer.py
+++ author/serializer.py
@@ -3,8 +3,7 @@ from rest_framework import serializers
 from author.models import Author
 
 class AuthorSerializer(serializers.ModelSerializer):
-    type = 'author'
     class Meta:
         model = Author
-        fields = ['id', 'host', 'url','displayName', 'github', 'profileImage']
+        fields = ['id', 'type', 'host', 'url','displayName', 'github', 'profileImage']
         read_only_fields = ['type']
