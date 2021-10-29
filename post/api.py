9799f5e565c1d6f350f93d8ddf352302deccf43a
Nishtha Pant <pant1@ualberta.ca>
2021-10-29 04:56:03 -0600

    added docs for posts

--- post/api.py
+++ post/api.py
@@ -9,6 +9,8 @@ from rest_framework.response import Response
 from knox.auth import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.core.paginator import Paginator
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 import uuid
 import ast
@@ -18,6 +20,73 @@ class PostViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    @swagger_auto_schema(
+        operation_description="GET /service/author/< AUTHOR_ID >/posts/< POST_ID >",
+        responses={
+            "200": openapi.Response(
+                description="OK",
+                examples={
+                   "application/json": {
+                        "type":"post",
+                        "title":"A post title about a post about web dev",
+                        "id":"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/764efa883dda1e11db47671c4a3bbd9e",
+                        "source":"http://lastplaceigotthisfrom.com/posts/yyyyy",
+                        "origin":"http://whereitcamefrom.com/posts/zzzzz",
+                        "description":"This post discusses stuff -- brief",
+                        "contentType":"text/plain",
+                        "content":"Þā wæs on burgum Bēowulf Scyldinga, lēof lēod-cyning, longe þrāge folcum gefrǣge (fæder ellor hwearf, aldor of earde), oð þæt him eft onwōc hēah Healfdene; hēold þenden lifde, gamol and gūð-rēow, glæde Scyldingas. Þǣm fēower bearn forð-gerīmed in worold wōcun, weoroda rǣswan, Heorogār and Hrōðgār and Hālga til; hȳrde ic, þat Elan cwēn Ongenþēowes wæs Heaðoscilfinges heals-gebedde. Þā wæs Hrōðgāre here-spēd gyfen, wīges weorð-mynd, þæt him his wine-māgas georne hȳrdon, oð þæt sēo geogoð gewēox, mago-driht micel. Him on mōd bearn, þæt heal-reced hātan wolde, medo-ærn micel men gewyrcean, þone yldo bearn ǣfre gefrūnon, and þǣr on innan eall gedǣlan geongum and ealdum, swylc him god sealde, būton folc-scare and feorum gumena. Þā ic wīde gefrægn weorc gebannan manigre mǣgðe geond þisne middan-geard, folc-stede frætwan. Him on fyrste gelomp ǣdre mid yldum, þæt hit wearð eal gearo, heal-ærna mǣst; scōp him Heort naman, sē þe his wordes geweald wīde hæfde. Hē bēot ne ālēh, bēagas dǣlde, sinc æt symle. Sele hlīfade hēah and horn-gēap: heaðo-wylma bād, lāðan līges; ne wæs hit lenge þā gēn þæt se ecg-hete āðum-swerian 85 æfter wæl-nīðe wæcnan scolde. Þā se ellen-gǣst earfoðlīce þrāge geþolode, sē þe in þȳstrum bād, þæt hē dōgora gehwām drēam gehȳrde hlūdne in healle; þǣr wæs hearpan swēg, swutol sang scopes. Sægde sē þe cūðe frum-sceaft fīra feorran reccan",
+                        "author":{
+                            "type":"author",
+                            "id":"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+                            "host":"http://127.0.0.1:5454/",
+                            "displayName":"Lara Croft",
+                            "url":"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+                            "github": "http://github.com/laracroft",
+                            "profileImage": "https://i.imgur.com/k7XVwpB.jpeg"
+                        },
+                        "categories":["web","tutorial"],
+                        "count": 1023,
+                        "comments":"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/de305d54-75b4-431b-adb2-eb6b9e546013/comments",
+                        "commentsSrc":{
+                            "type":"comments",
+                            "page":1,
+                            "size":5,
+                            "post":"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/764efa883dda1e11db47671c4a3bbd9e",
+                            "id":"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/de305d54-75b4-431b-adb2-eb6b9e546013/comments",
+                            "comments":[
+                                {
+                                    "type":"comment",
+                                    "author":{
+                                        "type":"author",
+                                        "id":"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+                                        "url":"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+                                        "host":"http://127.0.0.1:5454/",
+                                        "displayName":"Greg Johnson",
+                                        "github": "http://github.com/gjohnson",
+                                        "profileImage": "https://i.imgur.com/k7XVwpB.jpeg"
+                                    },
+                                    "comment":"Sick Olde English",
+                                    "contentType":"text/markdown",
+                                    "published":"2015-03-09T13:07:04+00:00",
+                                    "id":"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/de305d54-75b4-431b-adb2-eb6b9e546013/comments/f6255bb01c648fe967714d52a89e8e9c",
+                                }
+                            ]
+                        },
+                        "published":"2015-03-09T13:07:04+00:00",
+                        "visibility":"PUBLIC",
+                        "unlisted":"false"
+                   }
+                }
+            ),
+            "404": openapi.Response(
+                description="Post not found",
+                examples={
+                    "application/json":{"detail": "post not found"}
+                }
+            ),
+        },
+        tags=['Check if Follower'],
+    )
     # GET a post with specified author id and post id
     def get_post(self, request, author_id=None, post_id=None):
         try:
@@ -114,6 +183,35 @@ class PostViewSet(viewsets.ModelViewSet):
                 "detail": e.args
             }
             return Response(response,status.HTTP_400_BAD_REQUEST)
+
+
+    @swagger_auto_schema(
+        operation_description="POST /service/author/< AUTHOR_ID >/posts/< POST_ID >",
+        responses={
+            "201": openapi.Response(
+                description="CREATED",
+                examples={
+                   "application/json": {
+                       
+                   }
+                }
+            ),
+            "405": openapi.Response(
+                description="Method not Allowed"
+            ),
+            "400": openapi.Response(
+                description="Bad Request",
+                examples={
+                    "application/json":{"detail": "Invalid visibility key"},
+                    "application/json":{"detail": "unlisted must be boolean"},
+                    "application/json":{"detail": "incorrect format for Categories"},
+                    "application/json":{"detail": "key(s) missing:", "message": "Error details..."},
+                }
+            ),
+
+        },
+        tags=['Delete an Author\'s Post'],
+    )
         
     # POST to create a post with generated post_id, PUT to put a post with specified post id
     def create_post(self, request, author_id=None, post_id=None):
@@ -181,6 +279,24 @@ class PostViewSet(viewsets.ModelViewSet):
         else:
             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
+    @swagger_auto_schema(
+        operation_description="DELETE /service/author/< AUTHOR_ID >/posts/< POST_ID >",
+        responses={
+            "200": openapi.Response(
+                description="OK",
+                examples={
+                   "application/json": {"detail": "Post deleted"}
+                }
+            ),
+            "404": openapi.Response(
+                description="Follower not found",
+                examples={
+                    "application/json":{"detail": "Post not found"}
+                }
+            ),
+        },
+        tags=['Delete an Author\'s Post'],
+    )
     def delete_post(self, request, author_id=None, post_id=None):
         try:
             post = Post.objects.get(author=author_id, id=post_id)
