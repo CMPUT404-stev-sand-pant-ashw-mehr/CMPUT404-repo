72a275af597dcd8ebf3010561b8a894a2fca36ef
flyrobot27 <stevenheung.ca@gmail.com>
2021-10-29 01:24:01 -0600

    Update Comment and Post model. uuid now does not have hyphens

--- comment/api.py
+++ comment/api.py
@@ -1,8 +1,8 @@
+from comment.models import Comment
 from author.models import Author
 from post.models import Post
-from rest_framework import status, viewsets, permissions, pagination
+from rest_framework import status, viewsets
 from .serializers import CommentSerializer
-from rest_framework.decorators import action
 from rest_framework.response import Response 
 
 from django.forms.models import model_to_dict
@@ -17,18 +17,12 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def get_post_comments(self, request, author_id=None, post_id=None):
         try:
-            author_query = Author.objects.exclude(user__isnull=True).get(id=author_id)
-            post_query = Post.objects.get(id=post_id, author=author_id)
+            Author.objects.exclude(user__isnull=True).get(id=author_id)
+            Post.objects.get(id=post_id, author=author_id)
         except:
             return Response({"detail": "post not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        author_detail = model_to_dict(author_query)
-
-        # replace the id field (used internally) to the url
-        author_detail['id'] = author_detail['url']
-
-        # remove the 'user' column, used internally for linking User model
-        author_detail.pop('user', None)
+        Comment.objects.filter()
 
     def add_comment_to_post(self, request, author_id=None, post_id=None):
         pass
