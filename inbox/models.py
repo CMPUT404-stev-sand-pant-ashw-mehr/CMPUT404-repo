85f88a978e4a9d3360f9eb5934415b9f335ab71d
Steven Heung <49768539+flyrobot27@users.noreply.github.com>
2021-11-17 15:17:19 -0700

    fixed the model id field where for every new migration a new default is generated (#73)
    * Removed default id uuid.uuid4 on id field. Because CharField doesn't work with uuid. It generates a new id for every migrations
    
    * Fixed problem in test case

---
+++ inbox/models.py
@@ -0,0 +1,43 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.db.models import Q
+from author.models import Author
+from post.models import Post
+from followers.models import FriendRequest
+from comment.models import Comment
+
+class Inbox(models.Model):
+    type = models.CharField(max_length=10, default="inbox")
+
+    author = models.ForeignKey(Author, related_name="owner", on_delete=models.CASCADE)
+
+    post_item = models.ForeignKey(Post, related_name="inbox_post", on_delete=models.CASCADE, blank=True, null=True)
+
+    comment_item = models.ForeignKey(Comment, related_name="inbox_comment", on_delete=models.CASCADE, blank=True, null=True)
+
+    follow_item = models.ForeignKey(FriendRequest, related_name="inbox_request", on_delete=models.CASCADE, blank=True, null=True)
+
+    def clean(self) -> None:
+        if not (self.post_item or self.comment_item or self.follow_item):
+            raise ValidationError("Item has to be either a post, comment or follow")
+        return super().clean()
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=(
+                    Q(post_item__isnull=False) &
+                    Q(comment_item__isnull=True) &
+                    Q(follow_item__isnull=True)
+                ) | (
+                    Q(post_item__isnull=True) &
+                    Q(comment_item__isnull=False) &
+                    Q(follow_item__isnull=True)
+                ) | (
+                    Q(post_item__isnull=True) &
+                    Q(comment_item__isnull=True) &
+                    Q(follow_item__isnull=False)
+                ),
+                name="only_one_item_type",
+            )
+        ]
---
+++ inbox/urls.py
@@ -0,0 +1,7 @@
+from django.urls.conf import path, re_path
+from inbox.api import InboxViewSet
+
+urlpatterns = [
+    re_path(r"^author/(?P<author_id>[a-z0-9-\.-]+)/inbox/?$", 
+            InboxViewSet.as_view({"get": "get_inbox", "post": "post_inbox", "delete": "delete_inbox"}))
+]
