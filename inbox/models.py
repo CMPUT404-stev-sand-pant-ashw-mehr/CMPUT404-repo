31cc0da5066d849af37ddce9e9702f0b5a4ff352
flyrobot27 <stevenheung.ca@gmail.com>
2021-11-20 23:41:40 -0700

    Removed commented code

--- inbox/models.py
+++ inbox/models.py
@@ -10,36 +10,3 @@ class Inbox(models.Model):
     author = models.ForeignKey(Author, related_name="owner", on_delete=models.CASCADE)
 
     inbox_items = ArrayField(models.JSONField(), default=list)
-
-    '''
-    post_item = models.ForeignKey(Post, related_name="inbox_post", on_delete=models.CASCADE, blank=True, null=True)
-
-    like_item = models.ForeignKey(Like, related_name="inbox_like", on_delete=models.CASCADE, blank=True, null=True)
-
-    follow_item = models.ForeignKey(FriendRequest, related_name="inbox_request", on_delete=models.CASCADE, blank=True, null=True)
-
-    def clean(self) -> None:
-        if not (self.post_item or self.like_item or self.follow_item):
-            raise ValidationError("Item has to be either a post, like or follow")
-        return super().clean()
-
-    class Meta:
-        constraints = [
-            models.CheckConstraint(
-                check=(
-                    Q(post_item__isnull=False) &
-                    Q(like_item__isnull=True) &
-                    Q(follow_item__isnull=True)
-                ) | (
-                    Q(post_item__isnull=True) &
-                    Q(like_item__isnull=False) &
-                    Q(follow_item__isnull=True)
-                ) | (
-                    Q(post_item__isnull=True) &
-                    Q(like_item__isnull=True) &
-                    Q(follow_item__isnull=False)
-                ),
-                name="only_one_item_type",
-            )
-        ]
-        '''
