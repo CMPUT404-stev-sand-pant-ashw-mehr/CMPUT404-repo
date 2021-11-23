8390d04b9f91995e5c1508ae0b127523ecd848f9
Jingyu Xiang <jxiang2@ualberta.ca>
2021-11-22 22:02:38 -0700

    work on like

--- likes/api.py
+++ likes/api.py
@@ -52,8 +52,8 @@ class PostLikeViewSet(viewsets.ModelViewSet):
         
         try:
             query_set = Post.objects.get(id=post_id).like_set.create(author=author_inst, object=request.build_absolute_uri().strip("/likes"))
-        except:
-            return Response({"message": "You have already liked the post!"}, status=status.HTTP_403_FORBIDDEN)
+        except Exception as e:
+            return Response({"message": e.args}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
         response = LikeSerializer(query_set).data
 
@@ -101,8 +101,8 @@ class CommentLikeViewSet(viewsets.ModelViewSet):
         
         try:
             query_set = Comment.objects.get(id=comment_id).like_set.create(author=author_inst, object=request.build_absolute_uri().strip("/likes"))
-        except:
-            return Response({"message": "You have already liked the comment!"}, status=status.HTTP_403_FORBIDDEN)
+        except Exception as e:
+            return Response({"message": e.args}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
         response = LikeSerializer(query_set).data
 
