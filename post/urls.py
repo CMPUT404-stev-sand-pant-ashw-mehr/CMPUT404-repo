0dc4dd5d206ee4acc8a162085fb8a7ef729fe712
flyrobot27 <stevenheung.ca@gmail.com>
2021-11-24 14:39:13 -0700

    Removed import

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, re_path, path
 from .api import PostViewSet
-from .api import get_posts as get_public_posts
 
 urlpatterns = [
     re_path(r'^author/(?P<author_id>[a-z0-9-:\.-]+)/posts/?$', PostViewSet.as_view({
