2f57f650b63c76049a545da7daf17ed7576a4ea2
flyrobot27 <stevenheung.ca@gmail.com>
2021-11-25 22:21:42 -0700

    Enclosed friends API in modelViewSet

--- followers/urls.py
+++ followers/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from followers.api import FollowerViewSet, get_friends
+from followers.api import FollowerViewSet, FriendViewSet
 
 urlpatterns = [
     re_path(r'^author/(?P<author_id>[a-z0-9:\.-]+)/followers/?$', FollowerViewSet.as_view({"get": "list"})),
@@ -11,5 +11,5 @@ urlpatterns = [
         # Example URL: http://127.0.0.1:8000/author/3aowe9uvgsao/followers/http://anthoerhost.com/author/f09sdg0bv0as9dfg
         #                                          { author_id }         {               foreign_author_id               }
         
-    path("author/<str:author_id>/friends", get_friends)
+    path("author/<str:author_id>/friends", FriendViewSet.as_view({"get": "get_friends"}))
 ]
