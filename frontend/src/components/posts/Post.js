352ea251175edda042e6488f489010976a0a8ee4
Jingyu Xiang <jxiang2@ualberta.ca>
2021-12-04 14:30:30 -0700

    fix frontend comments

--- frontend/src/components/posts/Post.js
+++ frontend/src/components/posts/Post.js
@@ -1,6 +1,5 @@
 import React, { Component, Fragment, useState, useEffect } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { getPost, createPostComment } from "../../actions/posts";
 import Moment from "react-moment";
@@ -11,7 +10,9 @@ import axios from "axios";
 export class Post extends Component {
   state = {
     commentContent: "",
-    author: null
+    author: null,
+    showComments: false,
+    comments:[]
   };
 
   renderPostContent = () => {
@@ -78,9 +79,30 @@ export class Post extends Component {
     this.getAuhorDetail()
   }
 
+  getComments = () => {
+    axios.get(this.props.post.comments, {auth:{username:'socialdistribution_t03',password:'c404t03'}})
+    .then(res => {
+      console.log(res.data.comments);
+      this.setState({ comments: res.data.comments })
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+  }
+
+  toggleComment = () => {
+    const { showComments } = this.state;
+    if (showComments) {
+      this.setState({ showComments:!showComments, comments:[] });
+    } else {
+      this.getComments();
+      this.setState({ showComments:!showComments });
+    }
+  }
+
   render() {
     const { post, commentContent } = this.props;
-    // console.log(post);
+    
     return (
       post && (
         <Fragment>
@@ -124,6 +146,19 @@ export class Post extends Component {
                 </button>
               </form>
             </div>
+          </div>
+          <div>
+          <button className="btn btn-primary" style={{margin:20}} onClick={this.toggleComment}> show comments </button>
+          {this.state.comments.map((item, index)=> 
+            <div style={{margin:10, border:'1px solid #a6a6a6'}} key={index} className="card">
+              <div className="card-header">@{item.author.displayName}</div>
+              <div className="card-body">
+                <p className="card-text" style={{fontSize:25}}>{item.comment}</p>
+                <p className="card-text secondary" style={{fontSize:10}}>Created at {item.published}</p>
+              </div>
+            </div>
+          )
+          }
           </div>
           <br />
           {post.commentsSrc.comments &&
