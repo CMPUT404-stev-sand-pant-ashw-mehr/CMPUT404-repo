d36883f8246b609efc54898fbc1fd72375def17d
flyrobot27 <stevenheung.ca@gmail.com>
2021-12-04 14:25:40 -0700

    Better author likes url routing

--- likes/urls.py
+++ likes/urls.py
@@ -3,7 +3,7 @@ from django.urls import re_path
 from .api import PostLikeViewSet, CommentLikeViewSet, AuthorLikeViewSet
 
 urlpatterns = [
-    re_path(r'^author/(?P<author_id>[a-z0-9-/:\.-]+)/post/(?P<post_id>[a-z0-9-:\.-]+)/likes/?$', PostLikeViewSet.as_view({
+    re_path(r'^author/(?P<author_id>[a-z0-9-\.-]+)/post/(?P<post_id>[a-z0-9-:\.-]+)/likes/?$', PostLikeViewSet.as_view({
             "get": "get_post_likes", 
             "post": "add_post_like",
             })),
@@ -11,7 +11,10 @@ urlpatterns = [
             "get": "get_comment_likes", 
             "post": "add_comment_like",
             })),
-    re_path(r'^author/(?P<author_id>[a-z0-9-/:\.-]+)/likes/?$', AuthorLikeViewSet.as_view({
+    re_path(r'^author/(?P<author_id>[a-z0-9-\.-]+)/likes/?$', AuthorLikeViewSet.as_view({
+            "get": "get_likes", 
+            })),
+    re_path(r'^author/(?P<author_id>(http://|https://)[a-z0-9\.-:]+(/author/)[a-z0-9\.-]+)/likes/?$', AuthorLikeViewSet.as_view({
             "get": "get_likes", 
             }))
 ]
